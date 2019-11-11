00d14b46d47a1a6752d6eca0e5e202292b44c4a4
shareef-sanaullah <57405273+shareef-sanaullah@users.noreply.github.com>
2019-11-11 21:17:52 +0300

    Add files via upload

---
+++ Batch6_PY_Day5.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+cars = ['bmw','audi','toyata','benz']
+
+cars.sort()
+
+print(cars)
+
+
+# In[2]:
+
+
+cars.sort(reverse=True)
+print(cars)
+
+
+# In[3]:
+
+
+bikes = ['bmw','harley','honda','hero','re']
+
+print(bikes)
+
+
+# In[4]:
+
+
+print('Here is the orginal list')
+print(bikes)
+
+print('\n Here is the sorted list')
+print(sorted (bikes))
+
+print('\n rechecking the original list')
+print(bikes)
+
+
+# In[5]:
+
+
+students =['abdul','satish','sneha','khaled','majid']
+
+len(students)
+
+
+# In[6]:
+
+
+my_students = ['shareef','hussain','shalini','pavan','john']
+
+for student in my_students:
+    print(student)
+
+
+# In[7]:
+
+
+for student in my_students:
+    print(f"{student.title()},Keep up the good practise")
+
+
+# In[12]:
+
+
+for student in my_students:
+    print(f"{student.title()},keep up the good practise")
+    
+print("\nThank you all for showing interest in learning python")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
